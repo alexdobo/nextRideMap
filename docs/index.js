8dd73cc7224fd63db7a41c61ef5dffeba96dd1fc
alexdobo <alexdobo@users.noreply.github.com>
2019-03-14 15:16:59 -0700

    improved icons and tooltip

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,19 @@
-//var url = 'https://nextride.whistler.bctransit.com/api/VehicleStatuses?patternIds[]=1024&patternIds[]=1036&patternIds[]=1039&patternIds[]=1086&patternIds[]=1045&patternIds[]=1047&patternIds[]=1067&patternIds[]=1069&patternIds[]=1079&patternIds[]=1091&patternIds[]=1092&patternIds[]=1077&patternIds[]=1087&patternIds[]=1021&patternIds[]=1044&patternIds[]=1063&patternIds[]=1098&patternIds[]=1099&patternIds[]=1061&patternIds[]=1078&patternIds[]=1080&patternIds[]=1097&patternIds[]=1032&patternIds[]=1029&patternIds[]=1057&patternIds[]=1059&patternIds[]=1016&patternIds[]=1033&patternIds[]=1046&patternIds[]=1056&patternIds[]=1058';
+// geo = {
+//     'type':'Feature',
+//     'geometry': {
+//         'type':'Point',
+//         'coordinates': [bus['lon'],bus['lat']]
+//     },
+//     'properties': {
+//         'name': bus['name'],
+//         'route': bus['route'],
+//         'time': bus['time'].timestamp(),
+//         'velocity': bus['v'],
+//         'bearing': bus['bearing'],
+//         'pID': bus['pID']
+//     }
+// }
+
 var map = L.map('map'),
     realtime = L.realtime({
         url: 'https://fzoaxgoahc.execute-api.us-east-1.amazonaws.com/default/nextRideMap',
@@ -24,7 +39,9 @@ var map = L.map('map'),
                     '</p>';
             }),
             l.bindTooltip(function(){
-                return f.properties.route
+                return '<p> Route: ' + f.properties.route + 
+                    '<br/> Speed: ' + f.properties.velocity +
+                    '</p>';
             })
             
         }
